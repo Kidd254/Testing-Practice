a2157d913d68057d4a319ce4f17cbaf046ea94b7
mastercoder <lorenkioko@gmail.com>
2023-02-09 12:16:35 +0300

    task 2 solution

--- .test.js
+++ .test.js
@@ -17,3 +17,22 @@ test('counts the number of characters in a string', () => {
     
     expect(result).toBe(expected);
 });
+
+//Task 2
+//function to be tested
+function reverseString(string) {
+    let individualLetters = string.split('');
+    let reversedArr = individualLetters.reverse();
+    let joinedReversed = reversedArr.join('');
+    return joinedReversed;
+}
+
+test('reverse the letters in a string', () => {
+//arrange
+const string = 'lawrence';
+const expected = 'ecnerwal';
+//act
+const result = reverseString(string);
+//assert
+expect(result).toBe(expected);
+});
